78014051ec4dc0789876b23a37907553fad29292
KJMvanderHorst <115348690+KJMvanderHorst@users.noreply.github.com>
2023-06-02 18:56:30 +0200

    added controllertest

---
+++ BookAPIAssignment/bookapiassignment/src/test/OrderControllerTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Assertions.*;
+
+import com.group87.bookapiassignment.BookCatalog.Book;
+
+import org.junit.jupiter.api.Test;
+
+public class OrderControllerTest {
+    @Test
+    void testOrderController(){
+
+    }
+}
