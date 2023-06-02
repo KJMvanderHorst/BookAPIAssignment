e39c12126941a1d2877e312e2c90f59f40636793
KJMvanderHorst <115348690+KJMvanderHorst@users.noreply.github.com>
2023-06-02 15:33:45 +0200

    completed catalog

--- BookAPIAssignment/bookapiassignment/src/main/java/com/group87/bookapiassignment/BookCatalog/Catalog.java
+++ BookAPIAssignment/bookapiassignment/src/main/java/com/group87/bookapiassignment/BookCatalog/Catalog.java
@@ -9,11 +9,80 @@ public class Catalog {
 
     private ArrayList<Book> books;
 
-    public void createBook(String identificationCode, String title, String author, int publicationYear){
+    public void createBook(String identificationCode, String title, String author, String publicationYear){
         Book newBook = new Book(identificationCode, title, author, publicationYear);
         books.add(newBook);
     }
 
-    
+    public Book readBookByID(String identifier){
+        for(Book book:books){
+            if(book.getId().equals(identifier)){
+                return book;
+            }
+        }
+        return null;
+    }
+
+    public ArrayList<Book> readBooksByAuthor(String author){
+        ArrayList<Book> newBooks = new ArrayList<>();
+        for(Book book: books){
+            if(book.getAuthor().equals(author)){
+                newBooks.add(book);
+            }
+        }
+        if(newBooks.isEmpty()){
+            return null;
+        } else {
+            return newBooks;
+        }
+    }
+
+    public ArrayList<Book> readBooksByYear(String year){
+        ArrayList<Book> newBooks = new ArrayList<>();
+        for(Book book: books){
+            if(book.getPublicationYear().equals(year)){
+                newBooks.add(book);
+            }
+        }
+        if(newBooks.isEmpty()){
+            return null;
+        } else {
+            return newBooks;
+        }
+    }
+
+    public ArrayList<Book> readBooksByTitle(String title){
+        ArrayList<Book> newBooks = new ArrayList<>();
+        for(Book book: books){
+            if(book.getTitle().equals(title)){
+                newBooks.add(book);
+            }
+        }
+        if(newBooks.isEmpty()){
+            return null;
+        } else {
+            return newBooks;
+        }
+    }
+
+    public void updateBookIdentifier(String currentId, String newId){
+        readBookByID(currentId).setId(newId);
+    }
+
+    public void updateBookTitle(String currentId, String newTitle){
+        readBookByID(currentId).setTitle(newTitle);
+    }
+
+    public void updateBookAuthor(String currentId, String newAuthor){
+        readBookByID(currentId).setAuthor(newAuthor);
+    }
+
+    public void updateBookPublicationYear(String currentId, String newYear){
+        readBookByID(currentId).setPublicationYear(newYear);
+    }
+
+    public void deleteBook(String id){
+        books.remove(readBookByID(id));
+    }
 
 }
