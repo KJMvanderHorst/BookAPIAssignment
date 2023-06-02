e9b318e149aa701815ed6f30a88a8c6ae8240a0d
KJMvanderHorst <115348690+KJMvanderHorst@users.noreply.github.com>
2023-06-02 19:10:58 +0200

    finished all the unit tests

--- BookAPIAssignment/bookapiassignment/src/test/OrderControllerTest.java
+++ BookAPIAssignment/bookapiassignment/src/test/OrderControllerTest.java
@@ -1,12 +1,16 @@
 import org.junit.jupiter.api.Assertions.*;
 
 import com.group87.bookapiassignment.BookCatalog.Book;
+import com.group87.bookapiassignment.BookOrder.OrderController;
 
 import org.junit.jupiter.api.Test;
 
 public class OrderControllerTest {
+
     @Test
     void testOrderController(){
-
+        OrderController testOrderController = new OrderController();
+        testOrderController.orderBook(new Book("001", "Yes", "ME", "1901"), 100);
+        assertEquals(testOrderController.getOrders().get(0).getOrderQuantity(),100);
     }
 }
