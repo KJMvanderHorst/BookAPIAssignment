2cbdff3482f5d72c222b236eb59aaef0be1e1959
KJMvanderHorst <115348690+KJMvanderHorst@users.noreply.github.com>
2023-06-02 14:21:23 +0200

    Add files via upload

---
+++ BookAPIAssignment/bookapiassignment/src/test/java/com/group87/bookapiassignment/BookapiassignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group87.bookapiassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookapiassignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
